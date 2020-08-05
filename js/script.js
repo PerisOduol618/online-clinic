af493da501283867baf3710ec405bd3058edb0a9
priscillapepe <priscillaungai99@gmail.com>
2020-08-05 23:50:22 +0300

    added carousel,styled cs for carousel and its js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Activate carousel
+$("#myCarousel").carousel();
+
+// Enable carousel indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable carousel controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
